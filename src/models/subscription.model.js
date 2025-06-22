48301208f9d42ca7915edc914132ebb6d7c9ff02
yasarSayyad-Yaro <sayyadyasar60@gmail.com>
2025-06-22 15:45:12 +0530

    fix: correct import statement for user model in subscription model refactor: remove unused import in user controller

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { User } from "./user.model";
+import { User } from "./user.model.js";
 const subscriptionSchema=new mongoose.Schema({
     subscriber:{
         type:mongoose.Schema.Types.ObjectId,
